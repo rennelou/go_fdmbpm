1a337c1582e66093a5a44d591ca571b1f73b4cf6
rennelou <rennelou@outlook.com>
2021-01-21 03:07:12 -0300

    simplificando

--- types/recurrence-form.go
+++ types/recurrence-form.go
@@ -1,26 +1,25 @@
 package types
 
-type recurrenceFormParams struct {
-	lastValue ComplexTupla
-	values    []ComplexTupla
-}
-
 // GetAlphasBetas ...
 func GetAlphasBetas(w Waveguide, d []complex128, indexZ int) []ComplexTupla {
 
-	zeroTupla := ComplexTupla{Alpha: complex(0, 0), Beta: complex(0, 0)}
-	initialValue := recurrenceFormParams{zeroTupla, []ComplexTupla{}}
+	abcs := w.Getabcs(indexZ)
 
-	recFunc := NewRecFunc(w.NX, initialValue, func(index int, v IValue) IValue {
-		r := v.(recurrenceFormParams)
+	result := make([]ComplexTupla, 0)
+	alpha := complex(0, 0)
+	beta := complex(0, 0)
+	for i, abc := range abcs {
+		alpha, beta, result = func(lastAlpha complex128, lastBeta complex128, list []ComplexTupla) (complex128, complex128, []ComplexTupla) {
+			a := abc.A
+			b := abc.B
+			c := abc.C
 
-		a, b, c := w.Getabc(index, indexZ)
-		alpha := c / (b - a*r.lastValue.Alpha)
-		beta := (d[index] + a*r.lastValue.Beta) / (b - r.lastValue.Alpha)
+			_alpha := c / (b - a*lastAlpha)
+			_beta := (d[i] + a*lastBeta) / (b - lastAlpha)
 
-		newTupla := ComplexTupla{Alpha: alpha, Beta: beta}
-		return recurrenceFormParams{lastValue: newTupla, values: append(r.values, newTupla)}
-	})
+			return _alpha, _beta, append(list, ComplexTupla{Alpha: _alpha, Beta: _beta})
+		}(alpha, beta, result)
+	}
 
-	return recFunc.ExecRecFunc().(recurrenceFormParams).values
+	return result
 }
--- types/waveguide.go
+++ types/waveguide.go
@@ -11,14 +11,21 @@ type Waveguide struct {
 	q [][]complex128
 }
 
+// ABC ...
+type ABC struct {
+	A complex128
+	B complex128
+	C complex128
+}
+
 // NewWaveguide ...
 func NewWaveguide(_dx int, _nx int, _dz int, _nz int) Waveguide {
-	_s := make([][]complex128, _nx)
-	_q := make([][]complex128, _nx)
+	_s := make([][]complex128, _nz)
+	_q := make([][]complex128, _nz)
 
-	for i := 0; i < _nx; i++ {
-		_s[i] = make([]complex128, _nz)
-		_q[i] = make([]complex128, _nz)
+	for i := 0; i < _nz; i++ {
+		_s[i] = make([]complex128, _nx)
+		_q[i] = make([]complex128, _nx)
 	}
 
 	return Waveguide{
@@ -33,26 +40,25 @@ func NewWaveguide(_dx int, _nx int, _dz int, _nz int) Waveguide {
 
 // SInitialize ...
 func (w Waveguide) SInitialize(n float64) {
-	for i := 0; i < w.NX; i++ {
-		for j := 0; j < w.NZ; j++ {
+	for i := 0; i < w.NZ; i++ {
+		for j := 0; j < w.NX; j++ {
 			w.s[i][j] = complex(n, 0)
 		}
 	}
 }
 
-// Getabc ...
-func (w Waveguide) Getabc(xIndex int, zIndex int) (complex128, complex128, complex128) {
-	a, c := complex(1, 0), complex(1, 0)
-
+// Getabcs ...
+func (w Waveguide) Getabcs(zIndex int) []ABC {
 	boundaryCondition := complex(0, 0)
-	b := w.s[xIndex][zIndex] - boundaryCondition
-
-	if xIndex == 0 {
-		a = 0
-	}
-	if xIndex == w.NX-1 {
-		c = 0
+	result := []ABC{
+		{complex(0, 0), w.s[zIndex][0] - boundaryCondition, complex(1, 0)},
 	}
 
-	return a, b, c
+	for i := 1; i < w.NX-1; i++ {
+		result = append(result, ABC{complex(1, 0), w.s[zIndex][i] - boundaryCondition, complex(1, 0)})
+	}
+
+	result = append(result, ABC{complex(1, 0), w.s[zIndex][w.NX-1] - boundaryCondition, complex(0, 0)})
+
+	return result
 }
